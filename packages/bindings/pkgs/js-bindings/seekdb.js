bfefe4c652b8eb6dcda48e6709d32fa0323dcb70
dengfuping <fuping.dfp@antgroup.com>
2026-05-25 20:28:07 +0800

    fix(bindings): prepend PATH on Windows to load DLLs from libs/

--- packages/bindings/pkgs/js-bindings/seekdb.js
+++ packages/bindings/pkgs/js-bindings/seekdb.js
@@ -1,10 +1,24 @@
 const path = require("path");
+const fs = require("fs");
 const { ensureBindingsDownloaded } = require("./download.js");
+
+function requireNativeBinding(bindingsDir) {
+  if (process.platform === "win32") {
+    const sep = path.delimiter;
+    const libsDir = path.join(bindingsDir, "libs");
+    const prefix = fs.existsSync(libsDir)
+      ? `${bindingsDir}${sep}${libsDir}`
+      : bindingsDir;
+    const pathEnv = process.env.PATH || "";
+    process.env.PATH = pathEnv ? `${prefix}${sep}${pathEnv}` : prefix;
+  }
+  return require(path.join(bindingsDir, "seekdb.node"));
+}
 
 /** Sync load from same dir (npm package / local build). Returns null if not found. */
 function getNativeNodeBindingSync() {
   try {
-    return require(path.join(__dirname, "seekdb.node"));
+    return requireNativeBinding(__dirname);
   } catch {
     return null;
   }
@@ -29,7 +43,7 @@ async function getNativeBindingAsync() {
   _loadPromise = (async () => {
     try {
       const cacheDir = await ensureBindingsDownloaded();
-      _cachedBinding = require(path.join(cacheDir, "seekdb.node"));
+      _cachedBinding = requireNativeBinding(cacheDir);
       return _cachedBinding;
     } catch (err) {
       _loadPromise = null;
